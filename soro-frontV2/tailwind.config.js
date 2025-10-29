914b0cc1b58b1807fd3c57ef45545d40c6737eef
VicZambom <140662019+VicZambom@users.noreply.github.com>
2025-10-29 21:33:16 +0000

    style(font): set Poppins as default font

--- soro-frontV2/tailwind.config.js
+++ soro-frontV2/tailwind.config.js
@@ -1,30 +1,13 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", //
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: { //
-      colors: { //
-        'bgWhite': '#FFFFFF', // Cor de fundo da sidebar
-        'text': '#111827', // Cor do texto principal
-        'hover-bg': '#F3F4F6', // Cinza claro para hover (ajustado para mais sutileza)
-        'active-bg': '#E0E7FF', // Azul/Índigo bem claro para fundo ativo (ajustado)
-
-        'soro-primary': {
-          DEFAULT: '#003CFF', // Azul principal (mantido)
-          dark: '#002680', // Variação escura
-          light: '#E0E7FF', // Ex: indigo-100 do Tailwind padrão
-        },
-        'soro-danger': { 
-          DEFAULT: '#DC2626', 
-          dark: '#B91C1C', 
-        },
-      },
-      fontFamily: { //
-        sans: ['Poppins', 'ui-sans-serif', 'system-ui'], //
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], 
       },
     },
   },
